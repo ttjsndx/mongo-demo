51167c94b66680ca105610455ef372fb79a7f766
ttjsndx <243017037@qq.com>
2018-11-14 14:14:19 +0800

    提供五种$eval传输数据的方法

--- src/main/java/com/wwl/mongoDemo/controller/userController.java
+++ src/main/java/com/wwl/mongoDemo/controller/userController.java
@@ -2,12 +2,14 @@ package com.wwl.mongoDemo.controller;
 
 import com.mongodb.BasicDBObject;
 import com.mongodb.CommandResult;
-import org.bson.BsonDocument;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.ScriptOperations;
+import org.springframework.data.mongodb.core.script.ExecutableMongoScript;
+import org.springframework.data.mongodb.core.script.NamedMongoScript;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.bson.conversions.Bson;
+
 
 @RestController
 public class userController {
@@ -21,13 +23,29 @@ public class userController {
      */
     @RequestMapping(value = "/test")
     public String test(){
-//        CommandResult commandResult =
-//                mongoTemplate.getDb().doEval("db.user.aggregate([{$group:{_id:'$name',count:{$sum:'$age'}}}])");
+        String command = "db.user.aggregate([{$group:{_id:'$name',count:{$sum:'$age'}}}])";
         BasicDBObject bson = new BasicDBObject();
-        bson.put("$eval","db.user.aggregate([{$group:{_id:'$name',count:{$sum:'$age'}}}])");
-        Object object =  mongoTemplate.getDb().runCommand(bson);
+        bson.put("$eval",command);
 
-        System.out.println(object);
+        Object object1 = mongoTemplate.getDb().doEval(command);
+        Object object2 =  mongoTemplate.getDb().command(bson);
+        Object object3 = mongoTemplate.getCollection("$cmd").findOne(bson);
+
+        ScriptOperations operations = mongoTemplate.scriptOps();
+        ExecutableMongoScript script = new ExecutableMongoScript(command);
+        Object object4 = operations.execute(script);
+
+        /**
+         * call是调用system.js集合中方法的方法，传入参数是sysytem.js表中数据的主键值，
+         * 可在mongo shell中天插入或者使用如下代码插入。
+         * 插入一次后可直接使用
+         */
+//        ScriptOperations operations = mongoTemplate.scriptOps();
+//        String command = "function(){return db.user.aggregate([{$group:{_id:'$name',count:{$sum:'$age'}}}])}";
+//        NamedMongoScript namedMongoScript = new NamedMongoScript("user2",script);
+//        operations.register(namedMongoScript);
+        Object object5 = operations.call("user2");
+
         return "success";
     }
 }
